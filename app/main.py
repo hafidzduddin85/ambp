1dc016a0aaf755e86d8253965e8c259fcf0f2e1c
hafidzduddin85 <muhamadhafidzduddin@gmail.com>
2025-07-08 10:55:50 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -35,13 +35,13 @@ def get_db():
 def get_current_user(request: Request):
     user = request.session.get("user")
     if not user:
-        raise RedirectResponse(url="/login", status_code=302)
+        return RedirectResponse(url="/login", status_code=302)
     return user
 
 def get_admin_user(request: Request, db: Session = Depends(get_db)):
     username = request.session.get("user")
     if not username:
-        raise RedirectResponse(url="/login", status_code=302)
+        return RedirectResponse(url="/login", status_code=302)
     user = db.query(User).filter_by(username=username).first()
     if not user or user.role != "admin":
         raise HTTPException(status_code=403, detail="Unauthorized")
