a2706d6004556f2e088e468c3f5d1898ec1e9145
hafidzduddin85 <muhamadhafidzduddin@gmail.com>
2025-07-10 00:45:08 +0700

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -2,8 +2,8 @@
 
 from starlette.requests import Request
 
-def set_flash(request: Request, message: str):
-    request.session["flash"] = message
+def set_flash(request: Request, message: str, category: str = "info"):
+    request.session["_flash"] = {"message": message, "category": category}
 
 def get_flash(request: Request):
-    return request.session.pop("flash", None)
+    return request.session.pop("_flash", None)
