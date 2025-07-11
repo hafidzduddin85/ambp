1837abe0b268e1ec107d4bb16a52e40bc0c7a4a7
hafidzduddin85 <muhamadhafidzduddin@gmail.com>
2025-07-11 10:01:24 +0700

    disabel app.models.user

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,14 @@
+# This file is deprecated - User model moved to app/utils/models.py
+# Please import from: from app.utils.models import User
 # app/models/user.py
-from sqlalchemy import Column, Integer, String
-from app.database.base import Base
+#from sqlalchemy import Column, Integer, String
+#from app.database.base import Base
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    role = Column(String, default="user")
-    is_active = Column(String, default="true")
+#class User(Base):
+#    __tablename__ = "users"
+#
+#    id = Column(Integer, primary_key=True, index=True)
+#    username = Column(String, unique=True, index=True, nullable=False)
+#    hashed_password = Column(String, nullable=False)
+#    role = Column(String, default="user")
+#    is_active = Column(String, default="true")
